e7013a981b0e9d92a81110a5016479ae898c9cbb
Ubuntu <ubuntu@ip-172-31-32-97.eu-west-1.compute.internal>
2013-07-06 15:30:08 +0000

    Fixed filename missing post-quote

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var buffer = new Buffer();
 
-buffer = fs.readFileSync('index.html); 
+buffer = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
   response.send(buffer.toString());
